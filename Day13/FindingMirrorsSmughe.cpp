d250d07c3882432cf33368883113d44f07b6b4cf
De_Bruijckere Eek (Consultant) <Eek.de.Bruijckere@consultant.volvo.com>
2024-01-12 09:56:46 +0100

    Day13 part2 done

--- Day13/FindingMirrorsSmughe.cpp
+++ Day13/FindingMirrorsSmughe.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 void NextMap(fstream& input, vector<string>& map);
 void transform(vector<string> input, vector<string>& output);
+int compareOffset(string s1, string s2);
 
 void getNextMap(fstream& input, vector<string>& map){
     string buff;
@@ -32,20 +33,14 @@ void transform(vector<string> input, vector<string>& output){
     }
 }
 
-bool validMirror(vector<string> map, int start, int end){
-    bool valid = true;
+int validMirror(vector<string> map, int start, int end){
+    //bool valid = true;
+    int differences = 0;
     for (int i = start, j = end; i < j; i++, j--)
     {
-        if(map[i].compare(map[j]) != 0) valid = false;
-        //cout << i << " " << j << " " << valid << endl;
+        differences += compareOffset(map[i],map[j]);
     }
-
-    for (int i = start, j = end; i > j; i--, j++)
-    {
-        if(map[i].compare(map[j]) != 0) valid = false;
-        //cout << i << " " << j << " " << valid << endl;
-    }
-    return valid;
+    return differences;
 }
 
 int getMirrorValue(vector<string> map){
@@ -53,20 +48,42 @@ int getMirrorValue(vector<string> map){
     first = map[0];
     last = map[map.size()-1];
     int totalPoints = 0;
+    int differences = 0;
     //check if it is from top
     for (int i = 1; i < map.size()-1; i += 2)
     {
-        if(map[i].compare(first) == 0 && validMirror(map,0,i)){
-            //cout << first << " " << map[i] << " " << i << endl;
-            //cout << "f size " << ((i+1)/2) << endl;;
-            totalPoints += ((i+1)/2); 
+        differences = 0;
+        differences += compareOffset(map[i],first);
+        if(differences <= 1){
+            differences += validMirror(map,1,i-1);
+            if(differences == 1){
+                //cout << i << "a  " << differences << endl;
+                totalPoints += ((i+1)/2); 
+            }
         }
-        if(map[i].compare(last) == 0 && validMirror(map,i,map.size()-1)){
-            //cout << "l size " << ((map.size()+i)/2) << " " << i << endl;
-            totalPoints += ((map.size()+i)/2);
+
+        differences = 0;
+        differences += compareOffset(map[i],last);
+        if(differences <= 1){
+            differences += validMirror(map,i+1,map.size()-2);
+            if(differences == 1){
+                //cout << i << "  " << differences;
+                totalPoints += ((map.size()+i)/2); 
+            }
         }
     }
     return totalPoints;
+}
+
+int compareOffset(string s1, string s2){
+    int count = 0;
+    for (int i = 0; i < s1.length(); i++)
+    {
+        if(s1[i] != s2[i]){
+            count++;
+        }
+    }
+    return count;
 }
 
 int main(){
@@ -90,12 +107,18 @@ int main(){
         //cout << "Transforming" << endl;
         transform(map,mapT); 
         
+        for (auto i : mapT)
+        {
+            //cout << i << endl;
+        }
+        
+
         first = mapT[0];
         last = mapT[mapT.size()-1];
         points += getMirrorValue(mapT);
         
     }
-
+    //low 12361
     cout << "Points " << points << endl;
     input.close();
 }
