80a21ba70a8d41ac1486b7ee45334fb3dad57755
De_Bruijckere Eek (Consultant) <Eek.de.Bruijckere@consultant.volvo.com>
2024-01-16 11:20:26 +0100

    day15 part1 done

---
+++ Day15/HashDecoder.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int hashMath(std::string hash){
+    int value = 0;
+    for (int i = 0; i < hash.length(); i++)
+    {
+        value += hash[i];
+        value *= 17;
+        value %= 256;
+    }
+    return value;
+}
+
+int main(){
+    std::fstream input("example.txt");
+    std::string buff;
+    int res = 0, total = 0;
+    int count = 0;
+    cout << hashMath("HASH") << endl;
+    while (!input.eof())
+    {
+        count++;
+        std::getline(input,buff,',');
+        res = hashMath(buff);
+        //cout << res << " " << buff << endl;
+        total += res;
+    }
+    //low
+    //high 522685
+    cout << total << endl;
+}
